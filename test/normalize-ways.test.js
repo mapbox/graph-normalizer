33eaadb4a964c6584ebc7cd1ed4a0e691a274711
Mark Misener <markmisener@gmail.com>
2020-09-16 12:23:05 -0400

    support 64 bit ints

--- test/normalize-ways.test.js
+++ test/normalize-ways.test.js
@@ -27,6 +27,25 @@ test('normalize-ways cli', function (t) {
   });
 });
 
+test('normalize-ways cli with bigInt ids', function (t) {
+  execSync('mkdir -p ' + path.join(__dirname, '../test_output'));
+  var cmd = 'node ' + path.join(__dirname, '../bin/normalize-ways') +
+            ' --outputPath ' + path.join(__dirname, '../test_output/') +
+            ' --waysFile ' + path.join(__dirname, 'fixtures/osm-ways-7-113-50.geojson') +
+            ' --zoomLevel 14';
+
+  exec(cmd, function (err, stdout, stderr) {
+    t.notOk(err, 'did not throw an error');
+    t.notOk(stderr, 'did not write to stderr');
+    t.equals(fs.readdirSync(path.join(__dirname, '../test_output')).length, 1, 'Good number of files created.');
+    var a = fs.readFileSync(path.join(__dirname, '../test_output/13300211231112.json'), {encoding: 'utf8'});
+    var b = fs.readFileSync(path.join(__dirname, 'fixtures/normalize-ways/13300211231112.json'), {encoding: 'utf8'});
+    t.equals(a, b, 'The output file matches expected');
+    execSync('rm -rf ' + path.join(__dirname, '../test_output'));
+    t.end();
+  });
+});
+
 test('normalize-ways cli with tricky boundary way', function (t) {
   execSync('mkdir -p ' + path.join(__dirname, '../test_output'));
   var cmd = 'node ' + path.join(__dirname, '../bin/normalize-ways') +
