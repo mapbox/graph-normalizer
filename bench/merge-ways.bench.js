83f93b0dbe47fe26c2fe78a39939e8ff198d9164
markmisener <mark.misener@mapbox.com>
2025-06-10 11:27:45 -0700

    update dependencies and node version

--- bench/merge-ways.bench.js
+++ bench/merge-ways.bench.js
@@ -1,25 +1,25 @@
-'use strict';
 
-var Benchmark = require('benchmark');
-var normalizer = require('../');
-var fs = require('fs');
-var path = require('path');
 
-var small = JSON.parse(fs.readFileSync(path.join(__dirname, './fixtures/small.json')));
-var medium = JSON.parse(fs.readFileSync(path.join(__dirname, './fixtures/medium.json')));
-var large = JSON.parse(fs.readFileSync(path.join(__dirname, './fixtures/large.json')));
+const Benchmark = require('benchmark');
+const normalizer = require('../');
+const fs = require('fs');
+const path = require('path');
+
+const small = JSON.parse(fs.readFileSync(path.join(__dirname, './fixtures/small.json')));
+const medium = JSON.parse(fs.readFileSync(path.join(__dirname, './fixtures/medium.json')));
+const large = JSON.parse(fs.readFileSync(path.join(__dirname, './fixtures/large.json')));
 
 new Benchmark.Suite('merge-ways')
-  .add('merge-ways # small', function () {
-    normalizer.mergeWays(small);
-  })
-  .add('merge-ways # medium', function () {
-    normalizer.mergeWays(medium);
-  })
-  .add('merge-ways # large', function () {
-    normalizer.mergeWays(large);
-  })
-  .on('cycle', function (event) {
-    console.log(String(event.target));
-  })
-  .run();
+    .add('merge-ways # small', () => {
+        normalizer.mergeWays(small);
+    })
+    .add('merge-ways # medium', () => {
+        normalizer.mergeWays(medium);
+    })
+    .add('merge-ways # large', () => {
+        normalizer.mergeWays(large);
+    })
+    .on('cycle', (event) => {
+        console.log(String(event.target));
+    })
+    .run();
